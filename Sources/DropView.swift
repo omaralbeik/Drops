0a09849a23c5ed441f36c47015254ab916f49604
maiyama18 <myam0118@gmail.com>
2022-08-22 13:36:37 +0900

    Remove icon tint color specification

--- Sources/DropView.swift
+++ Sources/DropView.swift
@@ -161,7 +161,6 @@ internal final class DropView: UIView {
     view.translatesAutoresizingMaskIntoConstraints = false
     view.contentMode = .scaleAspectFit
     view.clipsToBounds = true
-    view.tintColor = UIAccessibility.isDarkerSystemColorsEnabled ? .label : .secondaryLabel
     return view
   }()
 
